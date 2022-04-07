0128d7f8e286fa5573725482325d6d2c05201de9
fred <fredwright95@yahoo.com>
2022-04-07 14:58:21 -0400

    created new view function for get music by id

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.music_list),
+    path('<int:pk>/', views.music_detail),
 ]
--- music/views.py
+++ music/views.py
@@ -4,6 +4,8 @@ from .serializers import MusicSerializer
 from .models import Music
 from rest_framework import status
 
+from music import serializers
+
 
 @api_view(['GET', 'POST'])
 def music_list(request):
@@ -18,3 +20,13 @@ def music_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
+
+@api_view(['GET'])
+def music_detail(request, pk):
+    try:
+        music = Music.objects.get(pk=pk)
+        serializer = MusicSerializer(music)
+        return Response(serializer.data)
+    except Music.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)   
+            
